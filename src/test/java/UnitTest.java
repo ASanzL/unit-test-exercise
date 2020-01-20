54fdcc5297b5394119f4a5cd2fa1a1247b26eac1
Andreas Sanz <andreas.sanz@hotmail.com>
2020-01-20 13:08:50 +0100

    Added unit test

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -2,13 +2,14 @@ import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class UnitTest {
 
     @Test
-    @DisplayName("Display name")
-    public void testMethod() {
-        assertEquals(1, 1);
+    public void roleShouldntBeEmpty() {
+        Admin admin = new Admin("admin", "admin");
+        assertTrue(admin.getRole() != null && admin.getRole().length() > 0);
     }
 
 }
